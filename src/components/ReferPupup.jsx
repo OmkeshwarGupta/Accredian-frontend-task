aebaa81468c7806028d2aaac077db2daebed14bf
Omkeshwar Gupta <om999g@gmail.com>
2024-07-09 18:18:43 +0530

    Update

--- src/components/ReferPupup.jsx
+++ src/components/ReferPupup.jsx
@@ -23,13 +23,13 @@ const ReferPopup = ({ refer, setRefer }) => {
                     <h2 className='mt-4 text-3xl font-bold'>Referral Form</h2>
                     <div className='flex flex-col justify-center items-start mt-4 space-y-2'>
                         <label htmlFor="name">Name:</label>
-                        <input onChange={handleChange} type="text" id="name" name="name" className='rounded-lg w-[350px] h-[30px] border-2 border-black bg-col5'/>
+                        <input onChange={handleChange} required type="text" id="name" name="name" className='rounded-lg w-[350px] h-[30px] border-2 border-black bg-col5'/>
                         <label htmlFor="email">Email:</label>
-                        <input onChange={handleChange} type="text" id="email" name="email" className='rounded-lg w-[350px] h-[30px] border-2 border-black bg-col5'/>
+                        <input onChange={handleChange} required type="text" id="email" name="email" className='rounded-lg w-[350px] h-[30px] border-2 border-black bg-col5'/>
                         <label htmlFor="phone">Phone:</label>
-                        <input onChange={handleChange} type="text" id="phone" name="phone" className='rounded-lg w-[350px] h-[30px] bg-col5 border-2 border-black'/>
+                        <input onChange={handleChange} required type="text" id="phone" name="phone" className='rounded-lg w-[350px] h-[30px] bg-col5 border-2 border-black'/>
                         <label htmlFor="message">Message:</label>
-                        <textarea onChange={handleChange} id="message" name="message" className='rounded-lg w-[350px] h-[80px] bg-col5 border-2 border-black'></textarea>
+                        <textarea onChange={handleChange} required id="message" name="message" className='rounded-lg w-[350px] h-[80px] bg-col5 border-2 border-black'></textarea>
                     </div>
                     <button className='bg-col2 px-[16px] py-[8px] rounded-md text-white border inline-block w-[236px] h-[64px] mt-4'>Submit</button>
                 </div>
